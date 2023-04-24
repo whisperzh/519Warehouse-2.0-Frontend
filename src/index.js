a4e0f7d696b1337253eccfa80682f062262d00b8
Renming Zhang <rmzhang@bu.edu>
2023-04-24 16:46:12 -0400

    updated google api in env file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import { BrowserRouter } from 'react-router-dom';
 
-const clientId=require("./google-api-secret.json").web.client_id;
+const clientId=process.env.REACT_APP_GOOGLE_API_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
